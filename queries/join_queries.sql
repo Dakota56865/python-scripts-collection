f17d15b8a5b6b0aadad7c94a90e73bcc6d337f1f
Dakota Smerglia <dakotasmerglia6@gmail.com>
2024-09-06 19:01:15 -0400

    Update join_queries.sql
    This file showcases more-complex SQL queries that involve joining multiple tables, which are commonly used in business analytics.

--- queries/join_queries.sql
+++ queries/join_queries.sql
@@ -1,1 +1,19 @@
+-- Query to find customers with the highest lifetime value
+SELECT customer.customer_id, customer.first_name, customer.last_name, SUM(payment.amount) AS total_spent
+FROM customer
+JOIN rental ON customer.customer_id = rental.customer_id
+JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY customer.customer_id
+ORDER BY total_spent DESC
+LIMIT 10;
+
+-- Query to analyze movie rental patterns by country
+SELECT country.country, COUNT(rental.rental_id) AS rental_count
+FROM country
+JOIN city ON country.country_id = city.country_id
+JOIN address ON city.city_id = address.city_id
+JOIN customer ON address.address_id = customer.address_id
+JOIN rental ON customer.customer_id = rental.customer_id
+GROUP BY country.country
+ORDER BY rental_count DESC;
 
